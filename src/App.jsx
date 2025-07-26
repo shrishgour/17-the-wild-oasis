5e917919829a3e535d8e706eea6fe53cdfbf9c8b
shrishgour <shrishgour02071992@gmail.com>
2025-07-26 09:41:55 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  background-color: yellow;
+`;
+
+function App() {
+  return (
+    <div>
+      <H1>The Wild Oasis</H1>
+    </div>
+  );
+}
+
+export default App;
