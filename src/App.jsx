3bed16cba5f2f02bbec5b2a897cd4e940930de9e
shrishgour <shrishgour02071992@gmail.com>
2025-07-27 10:21:13 +0530

    React hook form is in

--- src/App.jsx
+++ src/App.jsx
@@ -31,7 +31,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route element={<AppLayout />}>
-            <Route index element={<Navigate replace to="deshboard" />} />
+            <Route index element={<Navigate replace to="dashboard" />} />
             <Route path="dashboard" element={<Dashboard />} />
             <Route path="bookings" element={<Bookings />} />
             <Route path="cabins" element={<Cabins />} />
