58cb06edb97db94c93ef8eebb654e53ed3ad216a
shrishgour <shrishgour02071992@gmail.com>
2025-08-07 09:53:24 +0530

    Deploy

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import SpinnerMini from "../../ui/SpinnerMini";
 import { useLogin } from "./useLogin";
 
 function LoginForm() {
-  const [email, setEmail] = useState("shrishgour02071992@gmail.com");
-  const [password, setPassword] = useState("i@mShrish02");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const { login, isLoading } = useLogin();
 
